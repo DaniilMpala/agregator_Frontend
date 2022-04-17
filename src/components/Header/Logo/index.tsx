acf1a6f3218836870d83d78c1a430619f543807d
AleshaIdetRvat <alex967268@gmail.com>
2022-04-17 17:29:53 +0300

    Added header logo component

---
+++ src/components/Header/Logo/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from './Logo.module.css'
+import logoIcon from './logo-icon.svg'
+import classNames from "classnames";
+
+const logoTextStyle = classNames({
+  [styles['logo__text']]: true,
+  [styles['logo__text--green']]: true,
+})
+
+const Logo: React.FC = () => {
+  return (
+    <div className={styles.logo}>
+        <img className={styles['logo__image']} src={logoIcon} alt='logo'/>
+        <span className={logoTextStyle}>Easy</span>
+        <span className={styles['logo__text']}>Discounts</span>
+    </div>
+  )
+}
+
+export default Logo
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+
+import React from "react";
+import Logo from './Logo'
+
+const Header: React.FC = () => {
+  return (
+    <header>
+      <Logo />
+    </header>
+  )
+}
+
+export default Header
